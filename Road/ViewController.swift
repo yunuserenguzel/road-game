552581b920149f5e0afa64fe46d6e2c05ef9bf01
Yunus Eren Guzel <exculuber@gmail.com>
2015-08-28 21:41:44 +0300

    Initial Commit

---
+++ Road/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Road
+//
+//  Created by Yunus Eren Guzel on 28/08/15.
+//  Copyright (c) 2015 yeg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
