d0e98128cc226a0a0b2299a22de8efc01c157e90
Yunus Eren Guzel <exculuber@gmail.com>
2015-08-29 21:47:03 +0300

    ionicons font is added

---
+++ Road/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  Road
+//
+//  Created by Yunus Eren Guzel on 29/08/15.
+//  Copyright (c) 2015 yeg. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+
+  struct FontName {
+    static let Ionicons = "Ionicons"
+  }
+
+}
